837f4f74de3e2bfcce4a7f043043fd2585993870
Alessandro Capra <alessandro.cpr@gmail.com>
2017-11-28 14:47:21 +0100

    Added link to DB

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var pg = require('pg');
 const app = express()
 var port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log('Example app listening on port ' + port))
+app
+  .get('/', (req, res) => res.send('Hello World!'))
+  .get('/db', (request, response) =>  {
+    pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+      client.query('SELECT * FROM test_table', (err, result) => {
+        done();
+        if (err)
+         { console.error(err); response.send("Error " + err); }
+        else
+         { response.render('pages/db', {results: result.rows} ); }
+      })
+    })
+  })
+  .listen(port, () => console.log('Example app listening on port ' + port))
